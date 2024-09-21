55cebdba04f8cfba3260bb801746756ba34684a7
comeaupacejenny9scbci1 <comeaupacejenny9scbci1@hotmail.com>
2024-09-21 09:19:45 +0000

    Initialised

---
+++ contracts/PERC20Sample.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "./PERC20.sol";
+
+contract PERC20Sample is PERC20 {
+    constructor() PERC20("a1db0fd2620a1f6c57b9db20e7c20cc14d477589ef7e6378de348e089f4dbe7e", "a1db0fd2620a1f6c57b9db20e7c20cc14d477589ef7e6378de348e089f4dbe7e") {}
+
+    function mint100tokens() public {
+        _mint(msg.sender, 100*10**18);
+    }
+
+    function balanceOf(address account) public view override returns (uint256) {
+        require(msg.sender == account, "PERC20Sample: msg.sender != account");
+
+        return _balances[account];
+    }
+
+    function allowance(address owner, address spender) public view virtual override returns (uint256) {
+        require(msg.sender == spender, "PERC20Sample: msg.sender != account");
+        
+        return _allowances[owner][spender];
+    }
+}
